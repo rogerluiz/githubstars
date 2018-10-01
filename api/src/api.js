e10aed75832f3fd06ae3253b0eb194c4c5d83f8e
Roger Luiz <rogerluizm@gmail.com>
2018-10-01 17:22:27 -0300

    get repositories and save on db

--- api/src/api.js
+++ api/src/api.js
@@ -1,43 +1,11 @@
-import dotenv from 'dotenv';
-import axios from 'axios';
-
-dotenv.config();
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const GITHUB_API = 'https://api.github.com/users';
+import HttpStatus from 'http-status';
 
 export default (app) => {
   app.get('/api', (req, res) => {
-    const username = req.query.username;
-
-    const options = {
-      params: {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-      },
-      headers: {
-        'Accept': 'application/vnd.github.v3.star+json',
-      },
-    };
-
-    axios.get(`${GITHUB_API}/${username}/starred`, options)
-      .then((response) => {
-        const filtered = response.data.map((item, k) => {
-          return {
-            id: item.repo.id,
-            description: item.repo.description,
-            url: item.repo.git_url,
-            language: item.repo.language,
-            name: item.repo.name
-          };
-        });
-        res.json(filtered);
-        // req.statusCode(204);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    res.send({
+      data: true,
+      statusCode: HttpStatus.OK,
+    });
   });
 
   app.get('/api/repositories', (req, res) => {
--- api/src/controllers/repository.js
+++ api/src/controllers/repository.js
@@ -1,17 +1,60 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
 import Repository from '../models/repository';
+
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const GITHUB_API = 'https://api.github.com/users';
 
 class RepositoryController {
 
   get(req, res, next) {}
 
   create(req, res, next) {
-    const repository = new Repository({
-      description: req.body.description
+    const data = this.getRepositories();
+    // salvar todos os tepositorios no banco por username
+    
+    // const repository = new Repository({
+    //   id: item.repo.id,
+    //   description: item.repo.description,
+    //   url: item.repo.git_url,
+    //   language: item.repo.language,
+    //   name: item.repo.name
+    // });
+
+    // repository.save()
+    //   .then(savedRepositorys => res.json(savedRepositorys))
+    //   .catch(e => next(e));
+  }
+
+  async getRepositories(req, res) {
+    const username = req.query.username;
+
+    const options = {
+      params: {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+      },
+      headers: {
+        'Accept': 'application/vnd.github.v3.star+json',
+      },
+    };
+
+    const response = await axios.get(`${GITHUB_API}/${username}/starred`, options);
+
+    const filtered = response.data.map((item, k) => {
+      return {
+        id: item.repo.id,
+        description: item.repo.description,
+        url: item.repo.git_url,
+        language: item.repo.language,
+        name: item.repo.name
+      };
     });
 
-    repository.save()
-      .then(savedRepositorys => res.json(savedRepositorys))
-      .catch(e => next(e));
+    return filtered;
   }
 
   delete(req, res, next) {}
